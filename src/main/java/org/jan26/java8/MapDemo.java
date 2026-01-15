93d62038a43a511720ee61f0be015927a90816f0
khotAkash07 <khotakash5737@gmail.com>
2026-01-15 18:48:22 +0530

    Map with streamJava8

---
+++ src/main/java/org/jan26/java8/MapDemo.java
@@ -0,0 +1,17 @@
+package org.jan26.java8;
+
+import java.util.LinkedHashMap;
+
+public class MapDemo {
+    public static void main(String[] args) {
+
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+        map.put(101, "A");
+        map.put(102, "B");
+        map.put(103, "C");
+        map.put(104, "D");
+        map.put(105, "E");
+
+        map.entrySet().forEach(System.out::println);
+    }
+}
