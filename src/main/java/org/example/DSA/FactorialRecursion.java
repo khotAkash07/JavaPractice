1f70d4424fa1ab147f27408c88ceb88d998efe5e
khotAkash07 <khotakash5737@gmail.com>
2026-01-20 10:57:44 +0530

    FactorialByRecursion

---
+++ src/main/java/org/example/DSA/FactorialRecursion.java
@@ -0,0 +1,21 @@
+package org.example.DSA;
+
+import java.util.*;
+
+class FactCalculator{
+
+    public int fact(int n){
+        return n==1?1: fact(n-1) * n;
+    }
+}
+public class FactorialRecursion {
+    public static void main(String[] ars){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to find factorial :");
+        int result =  new FactCalculator().fact(sc.nextInt());
+        System.out.println(result);
+
+        sc.close();
+    }
+}
