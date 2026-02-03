d6f78125748bead86df3b77e10792d0fabdf00de
khotAkash07 <khotakash5737@gmail.com>
2026-02-03 17:07:30 +0530

    Single tone Design Pattern

---
+++ src/main/java/org/Feb26/dp/SingletonEx.java
@@ -0,0 +1,26 @@
+package org.Feb26.dp;
+
+class SingletonDemo{
+
+    private static SingletonDemo singletonDemo;
+
+    private SingletonDemo(){}
+
+    public static SingletonDemo getInstance(){
+        if(singletonDemo == null)
+            singletonDemo = new SingletonDemo();
+
+        return singletonDemo;
+    }
+}
+
+public class SingletonEx {
+
+    public static void main(String[] args) {
+        SingletonDemo demo1 = SingletonDemo.getInstance();
+        SingletonDemo demo2 = SingletonDemo.getInstance();
+
+        System.out.println(demo1.hashCode());
+        System.out.println(demo2.hashCode());
+    }
+}
